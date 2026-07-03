d1bae36489966c707fc048a1496d14916571a84c
vidit singh <singhvidit1917@gmail.com>
2026-07-03 15:23:33 +0530

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -136,11 +136,7 @@ apptForm.addEventListener("submit", async (e) => {
     }
 
     // reCAPTCHA check
-    const recaptchaResponse = typeof grecaptcha !== "undefined" ? grecaptcha.getResponse() : "";
-    if (!recaptchaResponse) {
-        alert("Please verify you are not a robot! ✅");
-        return;
-    }
+  
 
     const submitBtn = apptForm.querySelector(".form-submit");
     submitBtn.disabled = true;
